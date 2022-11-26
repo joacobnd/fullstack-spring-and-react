462c1d5095d110988ee7c7fa77086b521b5d47d5
joacobnd <joacomda@gmail.com>
2022-11-26 13:00:08 +0100

    Initial commit

---
+++ src/main/java/com/joaco/fullstackcourse/FullstackcourseApplication.java
@@ -0,0 +1,13 @@
+package com.joaco.fullstackcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaco/fullstackcourse/FullstackcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaco.fullstackcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
