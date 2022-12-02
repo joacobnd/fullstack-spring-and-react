1257d3ce40db215a6968af1fa9f786bc31b1b1f6
joacobnd <joacomda@gmail.com>
2022-12-02 12:48:34 +0100

    Databases and Spring Data JPA: @Table

--- src/main/java/com/joaco/fullstackcourse/student/Student.java
+++ src/main/java/com/joaco/fullstackcourse/student/Student.java
@@ -1,5 +1,6 @@
 package com.joaco.fullstackcourse.student;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @ToString
@@ -8,10 +9,28 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
