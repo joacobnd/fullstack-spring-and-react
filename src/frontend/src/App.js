9a78a099ac87935b4bf5d45b53eff8837943777d
joacobnd <joacomda@gmail.com>
2022-12-02 14:33:03 +0100

    Adding Students Feature: Student form

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -10,6 +10,7 @@ import {
     LoadingOutlined,
     PlusCircleOutlined
 } from '@ant-design/icons';
+import StudentDrawerForm from "./StudentDrawerForm";
 
 import './App.css';
 
@@ -39,12 +40,13 @@ const columns = [
     },
 ];
 
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
 function App() {
     const [students, setStudents] = useState([]);
     const [collapsed, setCollapsed] = useState(false);
     const [fetching, setFetching] = useState(true);
+    const [showDrawer, setShowDrawer] = useState(false);
 
     const fetchStudents = () =>
         getAllStudents()
@@ -65,57 +67,64 @@ function App() {
             return <Spin indicator={antIcon}/>;
         }
         if (students.length <= 0) {
-            return <Empty />;
+            return <Empty/>;
         }
-        return <Table dataSource={students}
-                      columns={columns}
-                      bordered
-                      title={() => <Button type="primary" shape="round" icon={<PlusCircleOutlined />} size="medium"> Add New Student </Button>}
-                      pagination={{pageSize: 50}} scroll={{y: 240}}
-                      rowKey={(student) => student.id}
-                      scroll={{y: 600}}
-            />;
-        }
+        return <>
+            <StudentDrawerForm
+                showDrawer={showDrawer}
+                setShowDrawer={setShowDrawer}
+            />
+            <Table dataSource={students}
+                   columns={columns}
+                   bordered
+                   title={() => <Button onClick={() => setShowDrawer(!showDrawer)} type="primary" shape="round"
+                                        icon={<PlusCircleOutlined/>} size="medium"> Add New Student </Button>}
+                   pagination={{pageSize: 50}} scroll={{y: 240}}
+                   rowKey={(student) => student.id}
+                   scroll={{y: 600}}
+            />
+        </>
+    }
 
-                      return <Layout style={{minHeight: '100vh'}}>
-            <Sider collapsible collapsed={collapsed}
-                   onCollapse={setCollapsed}>
-                <div className="logo"/>
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                    <Menu.Item key="1" icon={<PieChartOutlined/>}>
-                        Option 1
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<DesktopOutlined/>}>
-                        Option 2
-                    </Menu.Item>
-                    <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
-                        <Menu.Item key="3">Tom</Menu.Item>
-                        <Menu.Item key="4">Bill</Menu.Item>
-                        <Menu.Item key="5">Alex</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
-                        <Menu.Item key="6">Team 1</Menu.Item>
-                        <Menu.Item key="8">Team 2</Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="9" icon={<FileOutlined/>}>
-                        Files
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{padding: 0}}/>
-                <Content style={{margin: '0 16px'}}>
-                    <Breadcrumb style={{margin: '16px 0'}}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
-                        {renderStudents()}
-                    </div>
-                </Content>
-                <Footer style={{textAlign: 'center'}}>Joaquin N. Grandiccelli Design @2022</Footer>
-            </Layout>
+    return <Layout style={{minHeight: '100vh'}}>
+        <Sider collapsible collapsed={collapsed}
+               onCollapse={setCollapsed}>
+            <div className="logo"/>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1" icon={<PieChartOutlined/>}>
+                    Option 1
+                </Menu.Item>
+                <Menu.Item key="2" icon={<DesktopOutlined/>}>
+                    Option 2
+                </Menu.Item>
+                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
+                    <Menu.Item key="3">Tom</Menu.Item>
+                    <Menu.Item key="4">Bill</Menu.Item>
+                    <Menu.Item key="5">Alex</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="9" icon={<FileOutlined/>}>
+                    Files
+                </Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout className="site-layout">
+            <Header className="site-layout-background" style={{padding: 0}}/>
+            <Content style={{margin: '0 16px'}}>
+                <Breadcrumb style={{margin: '16px 0'}}>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                    {renderStudents()}
+                </div>
+            </Content>
+            <Footer style={{textAlign: 'center'}}>Joaquin N. Grandiccelli Design @2022</Footer>
         </Layout>
+    </Layout>
 }
 
 export default App;
