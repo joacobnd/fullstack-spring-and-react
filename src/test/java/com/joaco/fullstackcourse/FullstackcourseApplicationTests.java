9f3d9cf5905d096d1e49cdc0a37e6ab186b81b45
joacobnd <joacomda@gmail.com>
2022-12-05 11:15:06 +0100

    CI CD with Github Actions: make test fail

--- src/test/java/com/joaco/fullstackcourse/FullstackcourseApplicationTests.java
+++ src/test/java/com/joaco/fullstackcourse/FullstackcourseApplicationTests.java
@@ -1,5 +1,6 @@
 package com.joaco.fullstackcourse;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class FullstackcourseApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Oops test failed");
 	}
 
 }
